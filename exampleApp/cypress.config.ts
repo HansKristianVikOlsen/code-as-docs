5d6ea4211400c7b93d401e3abeaa49f846bb2683
HansKristianVikOlsen <hk-97@hotmail.com>
2023-02-12 19:52:05 +0100

    feat: add vite app

---
+++ exampleApp/cypress.config.ts
@@ -0,0 +1,55 @@
+// import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
+import {createEsbuildPlugin} from "@badeball/cypress-cucumber-preprocessor/esbuild";
+import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
+import { defineConfig } from "cypress";
+
+import pkg from '@badeball/cypress-cucumber-preprocessor';
+const { addCucumberPreprocessorPlugin } = pkg;
+
+// async function setupNodeEvents(on, config) {
+//   // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+//   await addCucumberPreprocessorPlugin(on, config);
+//   require("@cypress/code-coverage/task")(on, config);
+//   on("file:preprocessor", require("@cypress/code-coverage/use-babelrc"));
+//   on(
+//     "file:preprocessor",
+//     createBundler({
+//       plugins: [createEsbuildPlugin(config)]
+//     })
+//   );
+
+//   // Make sure to return the config object as it might have been modified by the plugin.
+//   return config;
+// }
+
+async function setupNodeEvents(on, config) {
+  await addCucumberPreprocessorPlugin(on, config);
+  on("file:preprocessor", require("@cypress/code-coverage/use-babelrc"));
+  on('file:preprocessor',     
+      createBundler({
+        plugins: [createEsbuildPlugin(config)],
+      })
+    );
+return config;
+}
+
+
+
+const cypressJsonConfig = {
+  fileServerFolder: ".",
+  fixturesFolder: "./src/fixtures",
+//  video: true,
+
+  chromeWebSecurity: false,
+  specPattern: "cypress/features/**/*.feature",
+//  supportFile: "src/support/e2e.ts",
+  viewportWidth: 1920,
+  viewportHeight: 1080
+  
+};
+export default defineConfig({
+  e2e: {
+    ...cypressJsonConfig,
+    setupNodeEvents: setupNodeEvents
+    },
+  });
---
+++ exampleApp/cypress/features/feature1/feature1.ts
@@ -0,0 +1,11 @@
+import { Given,When,Then } from "@badeball/cypress-cucumber-preprocessor";
+
+ Given(`I am a user`, () => {
+// cy.visit("/")
+ });
+ When(`I open the website`, () => {
+
+ });
+ Then(`I should be able to use feature1`, () => {
+
+});
